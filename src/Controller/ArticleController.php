24105c4b15a5d81a7ea867332966eda29fa55ea8
Krzysztof Bednarski <bednarski1978@gmail.com>
2019-11-03 08:38:08 +0100

    added twig show template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,13 +23,29 @@ class ArticleController
        }
 
     /**
-     * @Route ("/news/{slug}/{slug2}")
+ * @Route ("/news/{slug}/{slug2}")
+ * @param $slug
+ * @param $slug2
+ * @return Response
+ */
+    public function show($slug, $slug2){
+        return $this->render("article/show.html.twig",["title"=>"My first blog"]);
+//           return new Response("Future page to show the article " . $slug ." " . $slug2 );
+    }
+
+    /**
+     * @Route ("/news/{slug}")
      * @param $slug
-     * @param $slug2
      * @return Response
      */
-       public function show($slug, $slug2){
-           return new Response("Future page to show the article " . $slug ." " . $slug2 );
-       }
+    public function news($slug){
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render("article/show.html.twig",["title"=>"My first blog", "slug"=>$slug, "comments"=>$comments]);
+//           return new Response("Future page to show the article " . $slug ." " . $slug2 );
+    }
 
 }
