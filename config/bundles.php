4fde74896e868c6e4df89ecf957c733c09cb4b89
bedna <bednarski1978@gmail.com>
2019-11-24 10:13:30 +0100

    ArticleStatsCommand

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
