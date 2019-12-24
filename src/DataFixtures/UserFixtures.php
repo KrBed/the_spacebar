0f947c05d8bb88a6d2feb52a38cf7a372ab60a70
bedna <bednarski1978@gmail.com>
2019-12-24 08:57:23 +0100

    base controler and security roles

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -26,6 +26,17 @@ class UserFixtures extends BaseFixture
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
             $user->setPassword($this->passwordEncoder->encodePassword($user,'engage'));
+            $user->setTwitterUsername($this->faker->userName);
+
+            return $user;
+        });
+        $this->createMany(3, 'admin_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@thespacebar.com', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'engage'));
+            $user->setTwitterUsername($this->faker->userName);
+            $user->setRoles(["ROLE_ADMIN"]);
             return $user;
         });
         $manager->flush();
