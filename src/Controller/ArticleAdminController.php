91582673f26c33c3d6e281b07f52f0d272a90f1f
bedna <bednarski1978@gmail.com>
2019-12-27 19:08:07 +0100

    User authentication by ApiToken

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -19,8 +19,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleAdminController extends AbstractController
 {
-
-    
     /**
      * @Route("admin/article/new", name="admin_article_new")
      */
